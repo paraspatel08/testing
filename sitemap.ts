e7a05fd945fdc373eb33ea54f79710dfea9c2bf2
Prashant Patel <prashantpatelr47@gmail.com>
2025-04-03 19:04:20 +0530

    Add files via upload

---
+++ sitemap.ts
@@ -0,0 +1,43 @@
+import { MetadataRoute } from "next";
+import { config } from "@/config";
+
+interface SitemapURLS {
+  url: string;
+  lastModified: Date;
+  changeFrequency:
+    | "daily"
+    | "weekly"
+    | "always"
+    | "hourly"
+    | "monthly"
+    | "yearly"
+    | "never";
+  priority: number;
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const URL = config.site.mainURL;
+  const mainPath = ["", "fusion", "dex", "artists"];
+  const pages = ["self-fusions", "triple-fusions"];
+  const sitemapURLS: SitemapURLS[] = [];
+
+  mainPath.forEach((path) => {
+    sitemapURLS.push({
+      url: `${URL}/${path}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    });
+  });
+
+  pages.forEach((path) => {
+    sitemapURLS.push({
+      url: `${URL}/${path}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    });
+  });
+
+  return [...sitemapURLS];
+}
